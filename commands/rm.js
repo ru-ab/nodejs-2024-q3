a5e90e096b3715c33488673524e86b3805922f83
Rustam Abdrakhmanov <ruabdrakhmanov@gmail.com>
2024-10-14 21:16:57 +0600

    fix: remove unnecessary checking

--- commands/rm.js
+++ commands/rm.js
@@ -1,7 +1,5 @@
 import { rm } from 'node:fs/promises';
 import { InvalidInputError } from '../errors/InvalidInputError.js';
-import { isFile } from '../utils/isFile.js';
-import { isPathExists } from '../utils/isPathExists.js';
 
 export default async function ({ args }) {
   if (!args.length) {
@@ -9,10 +7,6 @@ export default async function ({ args }) {
   }
 
   const [pathToFile] = args;
-
-  if (!(await isPathExists(pathToFile)) || !(await isFile(pathToFile))) {
-    throw new InvalidInputError();
-  }
 
   await rm(pathToFile);
 }
